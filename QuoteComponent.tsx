691551861fc8211d020faef8c1627c01c409a368
Addison Schultz <addisonschultz0@gmail.com>
2018-11-17 13:04:08 +0100

    First commit to add quote code component

---
+++ QuoteComponent.tsx
@@ -0,0 +1,125 @@
+import * as React from "react";
+import { PropertyControls, ControlType } from "framer";
+
+// Define type of property
+interface Props {
+  img: string;
+  main: string;
+  company: string;
+  author: string;
+}
+
+function capitalize(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function error(e) {
+  e.currentTarget.src = "https://via.placeholder.com/150";
+}
+
+export class Quote extends React.Component<Props> {
+  // Set default properties
+  static defaultProps = {
+    height: 375,
+    width: 375,
+    img: "https://uinames.com/api/photos/male/17.jpg",
+    main: "Lorem Ipsum",
+    company: "Framer",
+    author: "Addison Schultz"
+  };
+
+  // Items shown in property panel
+  static propertyControls: PropertyControls = {
+    img: {
+      type: ControlType.String,
+      title: "Image URL"
+    },
+    main: {
+      type: ControlType.String,
+      title: "Quote Text"
+    },
+    company: {
+      type: ControlType.String,
+      title: "Company"
+    },
+    author: {
+      type: ControlType.String,
+      title: "Author"
+    }
+  };
+
+  render() {
+    return (
+      <div style={divStyle}>
+        <figure style={figureStyle}>
+          {this.props.img && (
+            <img style={quoteImgStyle} src={this.props.img} onError={error} />
+          )}
+          {this.props.img &&
+            this.props.company !== undefined &&
+            this.props.company !== null && (
+              <img
+                style={quoteSVGStyle}
+                src={`https://logo.clearbit.com/${this.props.company}.com`}
+                onError={error}
+              />
+            )}
+        </figure>
+
+        <p style={quoteMainStyle}>&quot;{this.props.main}&quot;</p>
+
+        <span style={spanStyle}>
+          <p style={quoteOtherStyle}>{this.props.author}</p>
+          {this.props.company && (
+            <p style={quoteOtherStyle}>, {capitalize(this.props.company)}</p>
+          )}
+        </span>
+      </div>
+    );
+  }
+}
+
+const divStyle: React.CSSProperties = {
+  padding: "20px",
+  width: "100%",
+  height: "100%",
+  textAlign: "center",
+  color: "black"
+};
+
+const spanStyle: React.CSSProperties = {
+  display: "inline-block"
+};
+
+const quoteMainStyle: React.CSSProperties = {
+  fontSize: "18px",
+  fontStyle: "italic"
+};
+
+const quoteOtherStyle: React.CSSProperties = {
+  display: "inline-block",
+  fontSize: "18px",
+  color: "#aaa"
+};
+
+const figureStyle: React.CSSProperties = {
+  textAlign: "center",
+  position: "relative",
+  paddingBottom: "5px"
+};
+
+const quoteImgStyle: React.CSSProperties = {
+  width: "50px",
+  height: "50px",
+  borderRadius: "100%"
+};
+
+const quoteSVGStyle: React.CSSProperties = {
+  position: "absolute",
+  left: "50%",
+  top: "30px",
+  border: "2px solid white",
+  width: "25px",
+  height: "25px",
+  borderRadius: "100%"
+};
